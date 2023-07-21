19a527f477c91e92e9e7c83a71869a0af0af2773
laupierre <laupierre@users.noreply.github.com>
2023-07-21 23:22:23 +0200

    Update deg_deseq2_analysis.R

--- deg_deseq2_analysis.R
+++ deg_deseq2_analysis.R
@@ -4,57 +4,71 @@ library(ggrepel)
 library (dplyr)
 
 
-## normal STAR results from the RNA-Seq IIT pipeline
+## normal STAR results from the human RNA-Seq IIT pipeline
 a <- read.delim ("star_gene_raw_counts.txt")
 annot <- a[ ,grep ("TMZ", colnames (a), invert=TRUE)]
+head (annot)
 
 counts <- a[ ,grep ("Gene|TMZ", colnames (a))]
 row.names (counts) <- counts$Geneid
 counts <- counts[ ,-1]
-head (counts)
 
 samples <- data.frame (matrix (nrow=dim (counts)[2], ncol=3))
 colnames (samples) <- c("sample", "cell", "condition")
 samples$cell <- gsub ("_.*", "", gsub ("TMZ_", "", colnames (counts)) )
-samples$condition <- gsub ("0-9", "", gsub (".*_", "", colnames (counts)))
-samples$sample <- row.names (samples)
+samples$condition <- gsub ("[0-9]", "", gsub (".*_", "", colnames (counts)))
+samples$sample <- colnames (counts)
 row.names (samples) <- colnames (counts)
 
 
-## See Github RNA-Seq_mouse/gene_annotation.R
-#system ("cp /projects/ncrrbt_share_la/dev_pipe/gencode.vM32.annotation.txt .")
+# U87
+samples.s <- samples[samples$cell == "U87", ]
+samples.s
 
-anno <- read.delim ("gencode.vM32.annotation.txt")
-
-anno <- anno[ ,grep ("transcript_id", colnames (anno), invert=TRUE)]
-anno <- unique (anno)
-
+counts.s <- counts[ ,colnames (counts) %in% samples.s$sample]
+stopifnot (colnames (counts.s) == samples.s$sample)
 
 
-## normal STAR results from the RNA-Seq IIT pipeline
-a <- read.delim ("subread.counts.txt", skip=1)
-a <- a[ ,grep ("Gene|bam", colnames (a))]
+## DESeq2 
+dds <- DESeqDataSetFromMatrix(countData = round (counts.s), colData = samples.s, design = ~ condition)
+                                 
+keep <- rowSums(counts(dds) >= 10) >= dim (counts.s)[2]/2
+dds <- dds[keep,]
+dds
 
-a <- merge (a, anno, by.x="Geneid", by.y="gene_id", all.x=TRUE) 
+dds$condition <- relevel(dds$condition, "C")
 
-a <- a[grep ("miRNA|Mt_tRNA|Mt_rRNA|rRNA|snRNA|snoRNA|scRNA|sRNA|misc_RNA|scaRNA|ribozyme|IG_|TR_", a$gene_type, invert=TRUE), ]
-colnames (a) <- gsub ("Aligned.out.bam", "", colnames (a))
-colnames (a) <- gsub ("star.", "", colnames (a))
+dds <- DESeq(dds)
+resultsNames(dds)
+
+res <- results(dds, contrast=list("condition_T_vs_C"))
+
+res <- merge (data.frame (res), round (counts (dds, normalized=TRUE)), by="row.names")
+res <- merge (res, annot, by.x="Row.names", by.y="Geneid", all.x= TRUE)
+colnames (res)[1] <- "Geneid"
+res <- res[order (res$padj), ]
+
+boxplot (res$log2FoldChange)
+abline (h=0)
+
+write.table (res, "TMZ_U87_differential_expression.txt", row.names=F, quote=F, sep="\t")
 
 
-counts <- annot <- a
+# PCA plot
+vsd <- vst(dds, blind=FALSE)
+pcaData <- plotPCA(vsd, intgroup=c("condition"), returnData=TRUE)
 
-annot <- annot[ ,c("Geneid", "gene_name", "gene_type", "mgi_id", "external_gene_name", "description")]
+percentVar <- round(100 * attr(pcaData, "percentVar"))
 
-row.names (counts) <- counts$Geneid
-counts <- counts[ ,grep ("IIT", colnames (counts))]
+p1 <- ggplot(pcaData, aes(PC1, PC2, color=condition, shape=condition)) +
+  		geom_point(size=3) +
+  		xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  		ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+		coord_fixed () + geom_label_repel (aes(label = name))
+p1
+ggsave ("PCA plot.pdf", p1)
 
-colnames (counts) <- gsub ("IIT_SHA_LINE1_KO_", "", colnames (counts))
-colnames (counts) <- gsub ("_S.*", "", colnames (counts))
 
-samples <- data.frame (matrix (nrow=dim (counts)[2], ncol=3))
-colnames (samples) <- c("sample", "condition", "area")
-samples$area <- "OB"
-samples$condition <- factor (c(rep ("KO",4), rep ("WT", 4)))
-row.names (samples) <- colnames (counts)
-samples$sample <- row.names (samples)
+
+
+
